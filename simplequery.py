a0a4f2d4e18586767994eb793bd975595aeae82f
CSchank <chris@schankula.ca>
2017-12-03 03:58:43 -0500

    attempt to fix options

--- simplequery.py
+++ simplequery.py
@@ -2,10 +2,10 @@ import urllib3, simplejson
 
 http = urllib3.PoolManager()
 
-#respv_pard = http.request('GET',"https://v3v10.vitechinc.com/solr/v_participant_detail/select?indent=on&wt=json" + "&q=*:*" + "&rows=1" + "&sort=WEIGHT%20asc" + "&fl=id,HEIGHT,WEIGHT")
-respv_par = http.request('GET', "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + "&q=*:*" + "&rows=1" + "&sort=latitude%20desc" + "&fl=id,DOB,city,sex,longitude,latitude,state")
+respv_pard = http.request('GET',"https://v3v10.vitechinc.com/solr/v_participant_detail/select?indent=on&wt=json" + "&q=*:*" + "&rows=1" + "&sort=ANNUAL_INCOME%20asc" + "&fl=id,ANNUAL_INCOME,WEIGHT")
+#respv_par = http.request('GET', "https://v3v10.vitechinc.com/solr/v_participant/select?indent=on&wt=json" + "&q=*:*" + "&rows=1" + "&sort=latitude%20desc" + "&fl=id,DOB,city,sex,longitude,latitude,state")
 
-v_par = simplejson.loads(respv_par.data.decode('utf-8'))
+v_par = simplejson.loads(respv_pard.data.decode('utf-8'))
 
 for document in v_par['response']['docs']:
     for doc_key in document:
