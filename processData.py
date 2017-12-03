7c152db5dfd25f1e66e002f5cec8731a679b0682
CSchank <chris@schankula.ca>
2017-12-03 04:39:46 -0500

    remove preconds loads

--- processData.py
+++ processData.py
@@ -60,7 +60,6 @@ stateL = ["NULL",
 stateDict = { stateL[i]: i for i in range(0,52) }
 
 def genCatStr(preconds):
-    preconds = simplejson.loads(preconds)
     labels = ['a00_b99_high', 'a00_b99_med', 'a00_b99_low',
     'c00_d49_high', 'c00_d49_med', 'c00_d49_low',
     'd50_d89_high', 'd50_d89_med', 'd50_d89_low',
