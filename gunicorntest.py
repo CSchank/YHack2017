00723563862c12b01416fc9b8bb1ee875d1aabd2
CSchank <chris@schankula.ca>
2017-12-02 22:04:03 -0500

    httptest add

--- gunicorntest.py
+++ gunicorntest.py
@@ -1,10 +1,19 @@
-def app(environ, start_response):
-    """Simplest possible application object"""
-    data = 'Goodbye world!\n'
-    status = '200 OK'
-    response_headers = [
-        ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return [bytes(data, 'utf-8')]
+import socketserver
+
+from http.server import BaseHTTPRequestHandler
+
+def some_function():
+    print ("some_function got called")
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/hello':
+            some_function()
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.send_response(200)
+        self.wfile.write(bytes("Hello!",'utf-8'))
+        return
+
+httpd = socketserver.TCPServer(("", 8080), MyHandler)
+httpd.serve_forever()
